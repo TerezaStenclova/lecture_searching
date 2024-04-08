b8194bfb78b86507787a236efd12e9487c89cefe
Stenclova tereza <terezka.stenclova@gmail.com>
2024-04-08 12:25:49 +0200

    novy

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -13,9 +14,27 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    #nacteni povolenych klicu
+    with open("sequential.json", "r") as f:
+        allowed_keys = json.load(f)["allowed_keys"]
+
+    #overeni zda je zdany klic povoleny
+    is field not in allowed_keys:
+    return None
+
+    #nacteni dat ze souboru file_name
+    with open(file_path, "r") as f:
+        data = json.load(f)
+
+    # vraceni hodnoty pod zadanym klicem
+    return data.get(field, None)
+
+
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print("Sequential data:", sequential_data)
+   # pass ????
 
 
 if __name__ == '__main__':
