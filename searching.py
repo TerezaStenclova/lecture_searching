c70126c92f787d85445a3679fe49fd4b05019b70
TerezaStenclova <terezka.stenclova@gmail.com>
2025-04-07 09:49:26 +0200

    pattern_search - done

--- searching.py
+++ searching.py
@@ -46,6 +46,25 @@ def linear_search(seq, num):
     }
     #nejlepsi ( O(1) ) vs. nejhorsi ( O(n) )
 
+def pattern_search(seq, pattern):
+    '''
+    Najde všechny pozice výskytu vzoru v DNA sekvenci.
+    :param sequence: (str) DNA sekvence
+    :param pattern: (str) hledaný vzor
+    :return: (set) množina pozic, kde vzor začíná
+    '''
+    delka = len(pattern)
+    pozice = set()
+    i = 0
+
+    #for i in range(len(seq)-delka+1): #Bez +1 bys přišla o poslední možné místo, kde by vzor mohl být #minus delka, abychom neprekrocili delku sekvence
+    while i <= len(seq) - delka:
+        if seq[i:i+delka] == pattern:
+            pozice.add(i)
+            i += 1
+        else:
+            i += 1 ## pokračování na další pozici při neshodě
+    return pozice
 
 def main():
     file_name = 'sequential.json' #pass
@@ -57,6 +76,14 @@ def main():
     vysledek = linear_search(sekvence, hledane_cislo)
     print(f"Hledané č.:{hledane_cislo} má tyto pozice: {vysledek['positions']} a nachází se v dané sekvenci čísel {vysledek['count']}krát.")
 
+    # Načtení DNA sekvence ze souboru
+    dna_data = read_data(file_name, field='dna_sequence')
+    print("DNA sekvence:", dna_data)
+
+    hledany_vzor = "ATA"
+    nalezene_pozice = pattern_search(dna_data, hledany_vzor)
+
+    print(f"Vzor '{hledany_vzor}' nalezen na pozicích: {nalezene_pozice}")
 
 if __name__ == '__main__':
     main()
